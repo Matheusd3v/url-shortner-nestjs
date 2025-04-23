795f8beab000668690340111eb977b704aaa0ec1
Matheus <>
2025-04-23 00:31:48 -0300

    feat: add url shortener entity

---
+++ src/app/modules/shortener/entities/url-shortener.entity.ts
@@ -0,0 +1,48 @@
+import { CommonEntity } from '@shared/classes/common-entity';
+
+export class UrlShortenerEntity extends CommonEntity {
+    private url: string;
+    private code: string;
+    private userId: number | null;
+
+    public setUrl(url: string) {
+        this.url = url;
+    }
+
+    public setCode(code: string) {
+        this.code = code;
+    }
+
+    public setUserId(id: number | null) {
+        this.userId = id;
+    }
+
+    public getUrl(): string {
+        return this.url;
+    }
+
+    public getCode(): string {
+        return this.code;
+    }
+
+    public getUserId(): number | null {
+        return this.userId;
+    }
+
+    static generateShortCode(length = 6) {
+        const chars =
+            'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * chars.length);
+            result += chars[randomIndex];
+        }
+
+        return result;
+    }
+
+    public getShortenedUrl(domain: string) {
+        return domain.concat(`/${this.code}`);
+    }
+}
