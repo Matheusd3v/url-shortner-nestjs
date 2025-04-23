6e2fe01473070cb99d760e2f11a73cdb5a8b80a9
Matheus <>
2025-04-23 00:37:03 -0300

    feat: implement url shortener in memory repository

---
+++ src/infra/database/in-memory/url-shortener-in-memory.repository.ts
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { Injectable } from '@nestjs/common';
+import { IUrlShortenerRepository } from '@modules/shortener/repositories/url-shortener.repository';
+import { IFindOptionsUrlShortenerDto } from '@modules/shortener/dtos/find-options.dto';
+import { UrlShortenerEntity } from '@modules/shortener/entities/url-shortener.entity';
+import { randomUUID } from 'crypto';
+
+@Injectable()
+export class UrlShortenerInMemoryRepository implements IUrlShortenerRepository {
+    private lastId: number = 0;
+    private database: UrlShortenerEntity[] = [];
+    private readonly getterMap = {
+        id: (entity: UrlShortenerEntity) => entity.getId(),
+        code: (entity: UrlShortenerEntity) => entity.getCode(),
+        uuid: (entity: UrlShortenerEntity) => entity.getUuid(),
+        userId: (entity: UrlShortenerEntity) => entity.getUserId(),
+    };
+
+    public async save(entity: UrlShortenerEntity): Promise<UrlShortenerEntity> {
+        entity.setId(this.getNewId());
+        entity.setUuid(randomUUID());
+        entity.setCreatedAt(new Date());
+        entity.setUpdatedAt(new Date());
+        entity.setDeletedAt(null);
+        this.database.push(entity);
+        return Promise.resolve(entity);
+    }
+
+    public async findOne(
+        args: IFindOptionsUrlShortenerDto,
+    ): Promise<UrlShortenerEntity | null> {
+        if (!args.where) return null;
+        const fields = Object.entries(args?.where);
+
+        return Promise.resolve(
+            this.database.find((entity) =>
+                fields.every(
+                    ([key, val]) => this.getterMap[key]?.(entity) === val,
+                ),
+            ) || null,
+        );
+    }
+
+    public async findAll(
+        args?: IFindOptionsUrlShortenerDto,
+    ): Promise<UrlShortenerEntity[]> {
+        if (!args?.where) return this.database;
+        const fields = Object.entries(args?.where);
+
+        return Promise.resolve(
+            this.database.filter((entity) =>
+                fields.every(
+                    ([key, val]) => this.getterMap[key]?.(entity) === val,
+                ),
+            ) || null,
+        );
+    }
+
+    private getNewId() {
+        this.lastId += 1;
+        return this.lastId;
+    }
+}
