9f3b1b28b74f78191d8a4bab25954e510d8fb899
Matheus <>
2025-04-23 03:21:41 -0300

    feat: change in memory db to postgresql

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ShortenerModule } from './app/modules/shortener/shortener.module';
+import { ClsPluginTransactional } from '@nestjs-cls/transactional';
+import { ClsModule } from 'nestjs-cls';
+import { PrismaModule } from './infra/database/sql/prisma/prisma.module';
+import { TransactionalAdapterPrisma } from '@nestjs-cls/transactional-adapter-prisma';
+import { PrismaPGService } from './infra/database/sql/prisma/prisma-pg.service';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
             envFilePath: './.env',
+        }),
+        ClsModule.forRoot({
+            plugins: [
+                new ClsPluginTransactional({
+                    imports: [PrismaModule],
+                    adapter: new TransactionalAdapterPrisma({
+                        prismaInjectionToken: PrismaPGService,
+                    }),
+                }),
+            ],
         }),
         ShortenerModule,
     ],
--- src/app/modules/shortener/shortener.module.ts
+++ src/app/modules/shortener/shortener.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UrlShortenerInMemoryRepository } from '@infra/database/in-memory/url-shortener-in-memory.repository';
 import { UrlShortenerController } from './controllers/url-shortener.controller';
 import { ShortenUrlUsecase } from './usecases/shorten-url.usecase';
 import { FindOriginalUrlUseCase } from './usecases/find-original-url.usecase';
+import { UrlPostgresqlRepository } from '@infra/database/sql/prisma/repository/url-repository';
 
 @Module({
     controllers: [UrlShortenerController],
     providers: [
         {
             provide: 'UrlRepository',
-            useClass: UrlShortenerInMemoryRepository,
+            useClass: UrlPostgresqlRepository,
         },
         ShortenUrlUsecase,
         FindOriginalUrlUseCase,
