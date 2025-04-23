882b805c992b10e8626b4842cbfd6fee35a06440
Matheus <>
2025-04-23 01:24:45 -0300

    feat: add route to redirect urls

--- src/app/modules/shortener/controllers/url-shortener.controller.ts
+++ src/app/modules/shortener/controllers/url-shortener.controller.ts
@@ -1,13 +1,26 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Get,
+    HttpStatus,
+    Param,
+    Post,
+    Res,
+} from '@nestjs/common';
 import { ShortenUrlRequestDto } from '../dtos/shorten-url-request.dto';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ShortedUrlResponseDto } from '../dtos/shorted-url-response.dto';
 import { ShortenUrlUsecase } from '../usecases/shorten-url.usecase';
+import { FindOriginalUrlUseCase } from '../usecases/find-original-url.usecase';
+import { Response } from 'express';
 
 @ApiTags('Url Shortener')
 @Controller()
 export class UrlShortenerController {
-    constructor(private readonly shortenUrlUseCase: ShortenUrlUsecase) {}
+    constructor(
+        private readonly shortenUrlUseCase: ShortenUrlUsecase,
+        private readonly findOriginalUrlUseCase: FindOriginalUrlUseCase,
+    ) {}
 
     @Post('/shorten')
     @ApiResponse({
@@ -18,5 +31,18 @@ export class UrlShortenerController {
         @Body() body: ShortenUrlRequestDto,
     ): Promise<ShortedUrlResponseDto> {
         return this.shortenUrlUseCase.execute(body);
+    }
+
+    @Get('/:code')
+    @ApiResponse({
+        status: HttpStatus.MOVED_PERMANENTLY,
+        description: 'Moves client to original url',
+    })
+    async redirectToOriginalUrl(
+        @Param('code') code: string,
+        @Res() res: Response,
+    ) {
+        const { url } = await this.findOriginalUrlUseCase.execute(code);
+        return res.redirect(HttpStatus.MOVED_PERMANENTLY, url);
     }
 }
--- src/app/modules/shortener/shortener.module.ts
+++ src/app/modules/shortener/shortener.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UrlShortenerInMemoryRepository } from '@infra/database/in-memory/url-shortener-in-memory.repository';
 import { UrlShortenerController } from './controllers/url-shortener.controller';
 import { ShortenUrlUsecase } from './usecases/shorten-url.usecase';
+import { FindOriginalUrlUseCase } from './usecases/find-original-url.usecase';
 
 @Module({
     controllers: [UrlShortenerController],
@@ -11,6 +12,7 @@ import { ShortenUrlUsecase } from './usecases/shorten-url.usecase';
             useClass: UrlShortenerInMemoryRepository,
         },
         ShortenUrlUsecase,
+        FindOriginalUrlUseCase,
     ],
 })
 export class ShortenerModule {}
